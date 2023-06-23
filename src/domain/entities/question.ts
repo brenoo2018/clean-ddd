0c80d137dd166e46fc00e8a05e14a0c4969bddf1
Taynan Breno <taynan.breno@oqvocequer.com.br>
2023-06-22 21:07:54 -0300

    value object of slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,15 +1,24 @@
 import { randomUUID } from 'node:crypto';
+import { Slug } from './value-objects/slug';
 
 export class Question {
   public id: string;
   public title: string;
+  public slug: Slug;
   public content: string;
   public authorId: string;
 
-  constructor(title: string, content: string, authorId: string, id?: string) {
+  constructor(
+    title: string,
+    content: string,
+    authorId: string,
+    slug: Slug,
+    id?: string
+  ) {
     this.title = title;
     this.content = content;
     this.authorId = authorId;
+    this.slug = slug;
     this.id = id ?? randomUUID();
   }
 }
