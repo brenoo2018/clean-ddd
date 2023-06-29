74283765166d9b1a8d92e68d4ba751b11b6bf262
Taynan Breno <taynan.breno@oqvocequer.com.br>
2023-06-29 00:29:39 -0300

    use case: create question

---
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,18 @@
+import { Question } from '../../enterprise/entities/question';
+import { QuestionsRepository } from '../repositories/question-repository';
+import { CreateQuestionUseCase } from './create-question';
+
+const fakeQuestionsRepository: QuestionsRepository = {
+  create: async (question: Question) => {},
+};
+
+test('create a question', async () => {
+  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository);
+  const { question } = await createQuestion.execute({
+    authorId: '1',
+    title: 'Nova pergunta',
+    content: 'conteúdo da pergunta',
+  });
+
+  expect(question.id).toBeTruthy();
+});
