d87842c972faa896612cee4d660352272ea51c65
Taynan Breno <taynan.breno@oqvocequer.com.br>
2023-07-07 11:27:57 -0300

    AggregateRoot base class

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,8 +1,8 @@
-import { Entity } from '@/core/entities/entity';
 import { Slug } from './value-objects/slug';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Optional } from '@/core/types/optional';
 import dayjs from 'dayjs';
+import { AgregateRoot } from '@/core/entities/agregate-root';
 
 export interface QuestionProps {
   authorId: UniqueEntityID;
@@ -14,7 +14,7 @@ export interface QuestionProps {
   updatedAt?: Date;
 }
 
-export class Question extends Entity<QuestionProps> {
+export class Question extends AgregateRoot<QuestionProps> {
   get authorId() {
     return this.props.authorId;
   }
